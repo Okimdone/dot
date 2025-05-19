ead2b23b15defd60ae5ffe102e711db4fa4f0cfd
Okimdone <abougadre@insea.ac.ma>
2025-05-19 14:27:11 +0100

    - adding the current hyperland config (init)

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -1,6 +1,9 @@
-pacman -S atuin nushell stow starship kitty
+pacman -S atuin nushell stow starship kitty hyprland
 yay -S carapace
 
 # nushell Setup
 mkdir ~/.local/share/atuin/
 atuin init nu | save ~/.local/share/atuin/init.nu
+
+# HyprLand Setup
+bash <(curl -s "https://end-4.github.io/dots-hyprland-wiki/setup.sh")
